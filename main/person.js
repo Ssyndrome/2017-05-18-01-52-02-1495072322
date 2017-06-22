e204260f68d947a2e930a3ec3f6b7b0ef5b71df4
Ssyndrome <915448715@qq.com>
2017-06-23 03:51:51 +0800

    Update person.js

--- main/person.js
+++ main/person.js
@@ -1,2 +1,8 @@
 // Write your code here
-module.exports = Person
+module.exports = function Person(name,age){
+  this.name = name;
+  this.age = age;
+  this.introduce = function(){
+    console.log("My name is "+name+".I am "+age+" years old.");
+  };
+};
